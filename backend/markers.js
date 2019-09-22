e34902464d94cb2bb77ebf83ae8778250ed8e2c5
duckranger <2087890+duckranger@users.noreply.github.com>
2019-09-23 10:07:51 +1200

    Updated comments. Removed unnecessary await and naming confusion

--- backend/markers.js
+++ backend/markers.js
@@ -5,9 +5,17 @@ import { get, update } from '@reshuffle/db';
  * 
  * Functions annotated with @expose are available for use in React components.
  * 
+ * A Map Marker (Marker) in this context comprises of:
+ * uid: number - Used as a unique key in the database.
+ * lat: number - Latitude on the map where the marker is positioned.
+ * lng: number - Longitude on the map where the marker is positioned. 
  */
 
+/**
+ * Key used to store the map markers details in the database.
+ */
 const markersKey = 'markers';
+
 /** 
  * Retrieve all markers from database. 
 */
@@ -20,30 +28,30 @@ export async function getMarkers() {
  * Save a new marker or update an existing one.
  * 
  * The function receives a marker object to persist,
- * containing a uid, and lat/lng attributes.
+ * containing a uid, and lat/lng attributes as described below.
  * 
- * @param {marker} - A marker object to persist.
+ * @param {number} lat - map latitude where the marker is placed.
+ * @param {number} lng - map longitude where the marker is placed.
+ * @param {number} uid - uid of the marker to update or undefined if it's a new marker. 
  * 
  */
 /* @expose */
-export async function saveMarker(marker) {
-  const { lat, lng, uid } = marker;
+export async function saveMarker({ lat, lng, uid }) {
   try {
     // Validate the uid for an existing marker, or create a new one
     // if the marker is brand new (denoted by a 0 uid)
     const validUid = await validateOrCreateUID(uid);
 
-    // Updatet the markers in the db, adding the new one to the list
-    const markers = await update(markersKey, (markers = {}) => {
+    // Update the markers in the db, adding the new one to the list
+    const updatedMarkers = await update(markersKey, (markers = {}) => {
       const allMarkers = { ...markers };
       allMarkers[validUid] = {
-        uid: validUid,
         lat,
         lng,
       };
       return allMarkers;
     });
-    return { markers: markers };
+    return { markers: updatedMarkers };
   } catch (error) {
     return { error: error.message };
   }
@@ -52,11 +60,12 @@ export async function saveMarker(marker) {
 /** 
  * Validates the uid, or creates a new one if the uid passed in is undefined.
  *
- * @param uid {number} - the uid to validate. Undefined if 
+ * @param {number} uid - uid to validate. If undefined is passed in - a new 
+ * uid will be returned. 
 */
 async function validateOrCreateUID(uid) {
   if (uid) {
     return uid;
   }
-  return await update('lastAllocatedUid', (v = 0) => v + 1);
+  return update('lastAllocatedUid', (v = 0) => v + 1);
 }
