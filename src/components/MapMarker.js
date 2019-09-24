022c242139fe8c396b9dd41ab823142d0fd86f52
duckranger <2087890+duckranger@users.noreply.github.com>
2019-09-24 17:16:24 +1200

    Map marker component which includes an InfoWindow to be displayed when the marker is clicked

---
+++ src/components/MapMarker.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {
+  Marker,
+  InfoWindow
+} from 'react-google-maps'
+
+export default function MapMarker(props) {
+  return (
+    <Marker
+      key={props.uid}
+      position={props.position}
+      onClick={() => props.setSelectedMarkerUid(props.uid)}
+    >
+
+      {
+        props.selected &&
+        <InfoWindow>
+          <div>
+            <button>
+              Delete
+            </button>
+          </div>
+        </InfoWindow>
+      }
+    </Marker>
+  )
+}
